791981686d9e2f3ecc17eece75e05cd4e3fac75d
Dhruv Pal <dhruvpal.0905@gmail.com>
2026-01-09 15:34:04 +0530

    add user model and hash password logic

---
+++ backend/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    }
+})
+
+UserSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(this.password, password)
+}
---
+++ backend/utils/Async-handler.js
@@ -0,0 +1,12 @@
+export const asyncHandlerWrapper = (fn) => {
+    return async (req, res, next) => {
+        try {
+            await fn(req, res, next)
+        } catch (err) {
+            res.status(err.statusCode || 500).json({
+                success: false,
+                message: err.message
+            })
+        }
+    }
+}
