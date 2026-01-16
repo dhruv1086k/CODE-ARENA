cd4a656330481d9ab1b6885ccacec4c6f0a17fd4
Dhruv Pal <dhruvpal.0905@gmail.com>
2026-01-17 01:13:15 +0530

    add filter + pagination in get todos route

--- backend/controllers/todo.controller.js
+++ backend/controllers/todo.controller.js
@@ -25,28 +25,37 @@ export const createTodo = asyncHandlerWrapper(async (req, res) => {
 })
 
 export const getTodos = asyncHandlerWrapper(async (req, res) => {
-    const page = req.query.page || 1
-    const limit = req.query.limit || 10
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
     const offset = (page - 1) * limit
+    const userId = req.user._id
+    const completed = req.query.completed
+    const filter = { owner: userId }
 
-    const userId = req.user._id.toString()
-    const todos = await Todo.find({ owner: userId })
-        .sort({ createdAt: -1 }) // give me newest record
-        .skip(offset) // how many docs to skip
-        .limit(limit) // how many items to serve
+    if (completed === 'true') {
+        filter.isCompleted = true
+    }
+    if (completed === 'false') {
+        filter.isCompleted = false
+    }
 
-    const totalTodos = await Todo.countDocuments({ owner: userId })
-    const totalPages = Math.ceil(totalTodos / limit)
+    const todo = await Todo.find(filter)
+        .sort({ createdAt: -1 })
+        .skip(offset)
+        .limit(limit)
+
+    const totalDocuments = await Todo.countDocuments(filter)
+    const totalPages = Math.ceil(totalDocuments / limit)
 
     return res
         .status(200)
         .json(
             new ApiResponse(200, {
-                todos,
+                todo,
                 "pagination": {
                     "page": page,
                     "limit": limit,
-                    "totalTodos": totalTodos,
+                    "totalTodos": totalDocuments,
                     "totalPages": totalPages
                 }
             }, "Todos fetched successfully")
